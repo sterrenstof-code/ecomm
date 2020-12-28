3ca54283ecc5fba9d2403ae46aa8f11320491511
Tom Coysman <65373256+sterrenstof-code@users.noreply.github.com>
2020-12-28 21:13:53 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -88,15 +88,26 @@ class UI {
          // add product to the cart
           cart = [...cart, cartItem];
          // save cart in local storage
-          Storage.saveCart(cart)
-         // update values in cart total
-
+          Storage.saveCart(cart);
+         // set cart values
+          this.setCartValues(cart);
          // display cart item
 
          // show the cart
 
         });
     });
+  }
+  setCartValue(cart){
+    let tempTotal = 0;
+    let itemsTotal = 0;
+    cart.map(item => {
+      tempTotal += item.price * item.amount;
+      itemsTotal += item.amount
+    })
+    cartTotal.innerText = parseFloat(tempTotal.toFixed(2))
+    cartItems.innerText = itemsTotal;
+    console.log(cartTotal, cartItems);
   }
 }
 
