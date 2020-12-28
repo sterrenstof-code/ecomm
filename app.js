8d56dba33b91d0f80bf5a04d71e39810f621e8b4
Tom Coysman <65373256+sterrenstof-code@users.noreply.github.com>
2020-12-28 20:01:16 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,84 @@
+
+/* shopping cart */
+
+//show cart when clicked
+
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDom = document.querySelector(".cart");
+const cartOverlay =  document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDom = document.querySelector(".products-center");
+
+
+// cart
+
+let cart = [];
+
+
+// Responsible for getting the products
+class Products {
+  async getProjects(){
+    try {
+      let result = await fetch("products.json");
+      let data = await result.json();
+      let products = data.items;
+      products  = products.map(item => {
+        const {title,price} = item.fields;
+        const {id} = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return {title, price, id, image};
+      })
+      return products;
+    } catch (error){
+      console.log(error);
+      
+    }
+  }
+}
+
+// display products (ui) getting the items that are returned from the project and displaying them
+
+class UI {
+  displayProducts(products){
+    let result = "";
+    products.forEach(product => {
+      result += `
+        <article class="product">
+          <div class="img-container">
+            <img
+              src=${product.image}
+              alt="product"
+              class="product-img"
+            />
+            <button class="bag-btn" data-id=${product.id}>
+              <i class="fas fa-shopping-cart"></i>
+              add to bag
+            </button>
+          </div>
+          <h3>${product.title}</h3>
+          <h4>${product.price}</h4>
+        </article> 
+      `
+    ;
+    });
+    productsDom.innerHTML = result; 
+  }
+}
+
+// local storage
+
+class Storage {
+
+}
+
+document.addEventListener("DOMContentLoaded", ()=> {
+   const ui = new UI()
+   const products = new Products()
+   // get all products
+   products.getProjects().then(products => ui.displayProducts(products));
+   
+});
